cae14cdc2cbf8660f9c695c1040ef6138a9faccf
yeseoz <w12345678992@gmail.com>
2023-04-04 12:13:20 +0900

    C# 첫번째 커밋

---
+++ Day01/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System; //include
+
+// 네임스페이스 ConsoleApp1
+namespace ConsoleApp1 // 파이썬의 패키지
+{
+    /// <summary> => xml 주석 만들어진 소스를 문서화 할때 자동화 하는 좋은 주석
+    /// 프로그램 클래스
+    /// </summary>
+    internal class Program // internal ==> private, public, protected (상속) 다 포함하는 기능
+    {               // 파일명과 클래스명을 동일하게 하는 것이 좋음
+        /// <summary>
+        /// 메인 엔트리 포인트
+        /// </summary>
+        /// <param name="args">콘솔 매개변수</param>
+        static void Main(string[] args) // 엔트리 포인트 시작점 ==>  void main()
+        {
+            Console.WriteLine("Hello C#"); // printf()
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Basic_CSharp_2023
 부경대 2023 객체지향프로그래밍 리포지토리
+
+## 1일차
+- C# 기본
+	- .NET framework / .NET 5.0 이후
+	- Visual Studio 상 c# 구성
+	- 기본 문법
